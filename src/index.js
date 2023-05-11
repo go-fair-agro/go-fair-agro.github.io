6321cd0f6c0a5ff1e784df9f5dad710a09ada733
gpbezerra <gabrielpbezerra8@gmail.com>
2023-05-11 10:29:45 -0300

    deploy fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from './Home';
 
+// deploy fix
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
