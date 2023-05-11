e995e34d37b2548e3820812f6441d3f7ef905f45
gpbezerra <gabrielpbezerra8@gmail.com>
2023-05-11 10:22:16 -0300

    deploy fix

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,25 @@
+import "./aboutUs.css";
+import logo from "../../assets/go-fair.png";
+
+export const AboutUs = () => {
+    return(
+        <div id="Rede" className="aboutUsContainer">
+            <div className="aboutUsText">
+                <div className="aboutUsRede">
+                    <h3>SOBRE A </h3>
+                    <h2>Rede GO FAIR</h2>
+                    <p>GO-FAIR é uma iniciativa global que promove a implantação dos princípios FAIR nas ações de governança e de gestão de dados e de objetos digitais, a fim de torná-los localizáveis, acessíveis, interoperáveis e reutilizáveis (do inglês FAIR - Findable, Accessible, Interoperable, Reusable).</p>
+                </div>
+                <div className="aboutUsImage">
+                    <img src={logo} alt="logo go fair agro brasil" />
+                </div>
+            </div>
+            <div className="aboutUsMissao">
+                <h2>NOSSA MISSÃO</h2>
+                <p>A missão da Rede GO-FAIR Brasil Agro consiste em trabalhar de modo articulado e colaborativo para fomentar o compartilhamento e reuso dos dados produzidos no âmbito dos sistemas produtivos agropecuários e também aqueles oriundos de pesquisas em ciências agrárias, bem como a adoção dos princípios FAIR. </p>
+                <p>Entre em nossa lista de discussão para para compartilhamento de informes, referências, eventos e outros assuntos de interesse da Rede GO FAIR Agro Brasil.</p>
+                <a id="buttonBlue" href="https://listas.rnp.br/mailman/listinfo/gofairbr-agro">Inscreva-se</a>
+            </div>
+        </div>
+    );
+}
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import  "./footer.css";
+import logo from "../../assets/logoFairAgro-noBG.png";
+import face from "../../assets/face.png";
+import insta from "../../assets/insta.png";
+import link from "../../assets/linkedin.png";
+
+export const Footer = () => {
+    return (
+        <div className="footerContainer">
+            <p>E-mail de contato: gofairagro@gmail.com</p>
+            <a href="https://r1.ufrrj.br/petsi/">Desenvolvido por PET-SI/UFRRJ</a>
+        </div>
+    )
+}
