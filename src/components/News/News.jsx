6656e1127a743ab672952e7ff72bbc1249e8f0c1
gpbezerra <gabrielpbezerra8@gmail.com>
2023-10-17 17:20:52 -0300

    adicionando do material da palesta do professor clement

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import pdf from "../../assets/23-10-17_OFAIRe_GO-FAIR-Agro-Brazil_Jonquet.pdf";
 import "./news.css";
 
 export const News = () => {
@@ -7,6 +8,13 @@ export const News = () => {
                 <h1>Novidades</h1>
 
                 <div className="postCardDeck">
+
+                <div className="newCard">
+                        <h2>Material da Palestra: FAIR DATA requires FAIR ONTOLOGIES - Disponível para Download</h2>
+                        <p style={{marginTop: 0}}>Estamos felizes em informar que a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet ocorreu com sucesso. Para aqueles que não puderam participar do evento ou desejam rever o conteúdo, estamos disponibilizando o material da apresentação para download. Clique no botão abaixo para baixar o material da palestra e aprofundar seus conhecimentos no tema de construção, organização, disponibilização e uso de ontologias de acordo com os princípios FAIR. Agradecemos a todos que participaram da palestra e esperamos que este recurso seja útil para sua pesquisa e estudos.</p>
+                        <a id="buttonGreenSmall" href={pdf} download>Baixar material</a>
+                    </div>
+
                     <div className="newCard">
                         <h2>Palestra com Clement Jonquet: Agroportal - Repositório de Vocabulários e Ontologias para Agronomia</h2>
                         <p style={{marginTop: 0}}>Estão abertas as inscrições para a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet na próxima terça-feira, dia 17 de outubro. Todos os interessados no tema de construção, organização, disponibilização e uso de ontologias poderão tirar proveito da fala do especialista francês que discutirá na sua palestra as questões relacionadas aos princípios FAIR e à construção e organização de ontologias, por meio da apresentação do AgroPortal e OntoPortal. A palestra é gratuita e será on-line, em inglês, somente para inscritos. Acesse o link e faça já sua inscrição</p>
@@ -33,3 +41,4 @@ export const News = () => {
         </div>
     );
 }
+
