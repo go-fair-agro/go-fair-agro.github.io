72f69589d84b0cc3e50de1211529688d376847a1
Gabriel Bezerra <gabrielpbezerra8@gmail.com>
2023-11-04 19:19:19 -0300

    tentando novamente o erro da página

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {HashRouter} from "react-router-dom";
+import {RouterProvider, createHashRouter} from "react-router-dom";
 import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
