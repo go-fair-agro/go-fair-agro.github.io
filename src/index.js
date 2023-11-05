0a9421b9c542322e62389900f859c1d9acd4a0d5
gpbezerra <gabrielpbezerra8@gmail.com>
2023-11-04 21:05:01 -0300

    testando outra solução para link

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {RouterProvider, createHashRouter} from "react-router-dom";
 import { App } from './App';
+import { HashRouter } from 'react-router-dom';
 
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+      <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
