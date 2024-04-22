029f7edda9548980f28f8e60df8f38972ac81106
gpbezerra <gabrielpbezerra8@gmail.com>
2024-04-22 10:40:47 -0300

    adicionando do nova noticia

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -13,16 +13,20 @@ export const News = () => {
                             <div className="postCarouselItem">
                                 <Carousel.Caption>
                                     <div className="newCardDeck">
+
+                                    <div className="newCard" style={{textAlign: "left"}}>
+                                            <h2>O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento.</h2>
+                                            <p style={{marginTop: 0}}>A REDE GO FAIR Agro e a Comissão Permanente em Vocabulários Controlados, Agroterminologias e Agrossemântica da Embrapa (GTermos) convidam para a mesa-redonda "O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento."</p>
+                                            <p style={{marginTop: 0}}>Todos os interessados no tema podem participar. As inscrições estão abertar e o evento será presencial e híbrido.</p>
+                                            <p style={{marginTop: 0}}><b>Evento presencial</b></p>
+                                            <p style={{marginTop: 0}}>Local: Embrapa Sede, Bloco D, Sala Paule Jeanne. Parque Estaçção Biológica - PqEB, s/nº, Brasília, DF.</p>
+                                            <a id="buttonGreenSmall" href="https://forms.gle/i8cPzQqTdP49bLf4A">Inscreva-se</a>
+                                        </div>
+
                                         <div className="newCard">
                                             <h2>Palestra com Clement Jonquet: Agroportal - Repositório de Vocabulários e Ontologias para Agronomia</h2>
                                             <p style={{marginTop: 0}}>Estão abertas as inscrições para a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet na próxima terça-feira, dia 17 de outubro. Todos os interessados no tema de construção, organização, disponibilização e uso de ontologias poderão tirar proveito da fala do especialista francês que discutirá na sua palestra as questões relacionadas aos princípios FAIR e à construção e organização de ontologias, por meio da apresentação do AgroPortal e OntoPortal. A palestra é gratuita e será on-line, em inglês, somente para inscritos.</p>
                                             {/* <a id="buttonGreenSmall" href="https://forms.gle/uY78KywEbQbnbAzc7">Inscreva-se</a> */}
-                                        </div>
-
-
-                                        <div className="newCard">
-                                            <h2>SBIAgro Aprova Nosso Trabalho: Desdobramentos da Rede GO FAIR Agro Brasil</h2>
-                                            <p style={{marginTop: 0}}>Grandes notícias! Nosso trabalho submetido ao SBIAgro, intitulado "Desdobramentos da Implementação da Rede GO FAIR Agro Brasil no Biênio 2021-2023", foi aprovado! Embora tenham sido solicitados pequenos ajustes, estamos ansiosos para finalizá-los. Estamos em contato com a organização do evento e conseguimos alocar 60 minutos no dia 8 de novembro, a partir das 15h, para apresentarmos nosso trabalho. Esta não será apenas uma apresentação em uma sessão técnica, mas sim uma sessão dedicada à Rede GO FAIR Agro Brasil. Fiquem ligados para mais atualizações!</p>
                                         </div>
                                     </div>
                                 </Carousel.Caption>
@@ -55,10 +59,13 @@ export const News = () => {
                             <div className="postCarouselItem">
                                 <Carousel.Caption>
                                     <div className="newCardDeck">
-                                        <div className="newCard">
-                                            <h2>Palestra com Clement Jonquet: Agroportal - Repositório de Vocabulários e Ontologias para Agronomia</h2>
-                                            <p style={{marginTop: 0}}>Estão abertas as inscrições para a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet na próxima terça-feira, dia 17 de outubro. Todos os interessados no tema de construção, organização, disponibilização e uso de ontologias poderão tirar proveito da fala do especialista francês que discutirá na sua palestra as questões relacionadas aos princípios FAIR e à construção e organização de ontologias, por meio da apresentação do AgroPortal e OntoPortal. A palestra é gratuita e será on-line, em inglês, somente para inscritos.</p>
-                                            {/* <a id="buttonGreenSmall" href="https://forms.gle/uY78KywEbQbnbAzc7">Inscreva-se</a> */}
+                                        <div className="newCard" style={{textAlign: "left"}}>
+                                            <h2>O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento.</h2>
+                                            <p style={{marginTop: 0}}>A REDE GO FAIR Agro e a Comissão Permanente em Vocabulários Controlados, Agroterminologias e Agrossemântica da Embrapa (GTermos) convidam para a mesa-redonda "O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento."</p>
+                                            <p style={{marginTop: 0}}>Todos os interessados no tema podem participar. As inscrições estão abertar e o evento será presencial e híbrido.</p>
+                                            <p style={{marginTop: 0}}><b>Evento presencial</b></p>
+                                            <p style={{marginTop: 0}}>Local: Embrapa Sede, Bloco D, Sala Paule Jeanne. Parque Estaçção Biológica - PqEB, s/nº, Brasília, DF.</p>
+                                            <a id="buttonGreenSmall" href="https://forms.gle/i8cPzQqTdP49bLf4A">Inscreva-se</a>
                                         </div>
                                     </div>
                                 </Carousel.Caption>
@@ -69,8 +76,9 @@ export const News = () => {
                                 <Carousel.Caption>
                                     <div className="newCardDeck">
                                         <div className="newCard">
-                                            <h2>SBIAgro Aprova Nosso Trabalho: Desdobramentos da Rede GO FAIR Agro Brasil</h2>
-                                            <p style={{marginTop: 0}}>Grandes notícias! Nosso trabalho submetido ao SBIAgro, intitulado "Desdobramentos da Implementação da Rede GO FAIR Agro Brasil no Biênio 2021-2023", foi aprovado! Embora tenham sido solicitados pequenos ajustes, estamos ansiosos para finalizá-los. Estamos em contato com a organização do evento e conseguimos alocar 60 minutos no dia 8 de novembro, a partir das 15h, para apresentarmos nosso trabalho. Esta não será apenas uma apresentação em uma sessão técnica, mas sim uma sessão dedicada à Rede GO FAIR Agro Brasil. Fiquem ligados para mais atualizações!</p>
+                                            <h2>Palestra com Clement Jonquet: Agroportal - Repositório de Vocabulários e Ontologias para Agronomia</h2>
+                                            <p style={{marginTop: 0}}>Estão abertas as inscrições para a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet na próxima terça-feira, dia 17 de outubro. Todos os interessados no tema de construção, organização, disponibilização e uso de ontologias poderão tirar proveito da fala do especialista francês que discutirá na sua palestra as questões relacionadas aos princípios FAIR e à construção e organização de ontologias, por meio da apresentação do AgroPortal e OntoPortal. A palestra é gratuita e será on-line, em inglês, somente para inscritos.</p>
+                                            {/* <a id="buttonGreenSmall" href="https://forms.gle/uY78KywEbQbnbAzc7">Inscreva-se</a> */}
                                         </div>
                                     </div>
                                 </Carousel.Caption>
