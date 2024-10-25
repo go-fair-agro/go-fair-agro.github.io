f3b8296c2f976d04ed00e429a5f561d97918278c
gpbezerra <gabrielpbezerra8@gmail.com>
2024-10-25 14:25:55 -0300

    nova noticia e palestra

--- src/components/Lecture/Lecture.jsx
+++ src/components/Lecture/Lecture.jsx
@@ -1,11 +1,15 @@
 import palestra1 from "../../assets/palestra1.jpeg";
 import palestra2 from "../../assets/palestra2.png";
 import palestra3 from "../../assets/palestra3.png";
+import palestra4 from "../../assets/palestra4.png";
+
 import ytb from "../../assets/ytb.png";
-import pdf from "../../assets/download.png";
-import pdf2 from "../../assets/23-10-17_OFAIRe_GO-FAIR-Agro-Brazil_Jonquet.pdf";
-import pdf3 from "../../assets/PalestraEmbrapa-Fernanda-Farinelli.pdf";
-import pdf4 from "../../assets/24-04-24_Embrapa_OntoPortal_Jonquet.pdf";
+import pdfPng from "../../assets/download.png";
+import pdf1 from "../../assets/23-10-17_OFAIRe_GO-FAIR-Agro-Brazil_Jonquet.pdf";
+import pdf2 from "../../assets/PalestraEmbrapa-Fernanda-Farinelli.pdf";
+import pdf3 from "../../assets/24-04-24_Embrapa_OntoPortal_Jonquet.pdf";
+import pdf4 from "../../assets/2024-10-17-Tools_to_characterize_FAIR_and_measure_FAIRness.pdf";
+
 
 
 import "./lecture.css";
@@ -17,8 +21,24 @@ export const Lecture = () => {
             <div className="cycleOficinasContainer">
                 <div className="cycleOficinasDeck">
 
+                    <div className="cycleCard">
+                        <p id="cycleCardTitle">Palestra com Leonardo Guerreiro Azevedo: Ferramentas para caracterizar e medir a adequação aos princípios FAIR (2024)</p>
+                        <img src={palestra4}  alt="" />
+                        <p>Acesse o material:</p>
+                        <div className="cardDeckLinks">
+                            <a target="blank" href="https://www.youtube.com/watch?v=dinb7D717-k">
+                                <img id="linkYtb" src={ytb} alt="" />
+                                <p>Youtube</p>
+                            </a>
+                            <a href={pdf4}  download>
+                                <img id="linkPDF" src={pdfPng} alt="" />
+                                <p>PDF</p>
+                            </a>
+                        </div>
+                    </div>
 
-                <div className="cycleCard">
+
+                    <div className="cycleCard">
                         <p id="cycleCardTitle">Palestra com Clement Jonquet: Ontology Repositories and Semantic Artefact Catalogues with the Ontoportal Technology (2024)</p>
                         <img src={palestra3} alt="" />
                         <p>Acesse o material:</p>
@@ -27,15 +47,15 @@ export const Lecture = () => {
                                 <img id="linkYtb" src={ytb} alt="" />
                                 <p>Youtube</p>
                             </a>
-                            <a href={pdf4} download>
-                                <img id="linkPDF"  src={pdf} alt="" />
+                            <a href={pdf3} download>
+                                <img id="linkPDF" src={pdfPng} alt="" />
                                 <p>PDF</p>
                             </a>
                         </div>
                     </div>
 
 
-                <div className="cycleCard">
+                    <div className="cycleCard">
                         <p id="cycleCardTitle">Palestra com Fernanda Farinelli: Conectando Mundos - Desafios Semânticos, Interoperabilidade e Ontologias de Domínio (2024)</p>
                         <img src={palestra2} alt="" />
                         <p>Acesse o material:</p>
@@ -44,8 +64,8 @@ export const Lecture = () => {
                                 <img id="linkYtb" src={ytb} alt="" />
                                 <p>Youtube</p>
                             </a>
-                            <a href={pdf3} download>
-                                <img id="linkPDF"  src={pdf} alt="" />
+                            <a href={pdf2} download>
+                                <img id="linkPDF" src={pdfPng} alt="" />
                                 <p>PDF</p>
                             </a>
                         </div>
@@ -60,17 +80,17 @@ export const Lecture = () => {
                                 <img id="linkYtb" src={ytb} alt="" />
                                 <p>Youtube</p>
                             </a>
-                            <a href={pdf2} download>
-                                <img id="linkPDF"  src={pdf} alt="" />
+                            <a href={pdf1} download>
+                                <img id="linkPDF" src={pdfPng} alt="" />
                                 <p>PDF</p>
                             </a>
                         </div>
                     </div>
-                    
-                    
+
+
 
                     <div id="palestraCard">
-                        <p style={{color: "#2D6D2C"}} id="cycleCardTitle">Clique nos links abaixo para conferir mais palestras:</p>
+                        <p style={{ color: "#2D6D2C" }} id="cycleCardTitle">Clique nos links abaixo para conferir mais palestras:</p>
                         <a target="blank" href="https://drive.google.com/file/d/1eAwKKLoillH0BnFzQC2y5leC-DbwFa2n/view?usp=sharing" id="linkPalestras">O Processo de Construção da Política de Governança de Dados, Informação e Conhecimento da Embrapa</a>
                         <a target="blank" href="https://drive.google.com/file/d/1Re3OZHgJXrEnhyek0coG1mAYaKlmiUAk/view?usp=sharing" id="linkPalestras">Plataformas para Criação de Planos de Gestão de Dados Usando os Princípios FAIR</a>
                         <a target="blank" href="https://drive.google.com/file/d/1i5FkAPpVD9baQkUThxNB0yCRMs2IjlvN/view?usp=sharing" id="linkPalestras">Planos de Gestão de Dados Acionáveis por Máquina Alinhados aos Princípios FAIR no LMB da Embrapa </a>
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -6,113 +6,95 @@ import Carousel from 'react-bootstrap/Carousel';
 export const News = () => {
     return (
         <div id="Novidades" className="postContainer">
-                <h1>Novidades</h1>
-                <div className="postCarouselDesktop">
-                    <Carousel data-bs-theme="dark">
-                        <Carousel.Item>
-                            <div className="postCarouselItem">
-                                <Carousel.Caption>
-                                    <div className="newCardDeck">
+            <h1>Novidades</h1>
+            <div className="postCarouselDesktop">
+                <Carousel data-bs-theme="dark">
+                    <Carousel.Item>
+                        <div className="postCarouselItem">
+                            <Carousel.Caption>
+                                <div className="newCardDeck">
 
-                                    <div className="newCard" style={{textAlign: "left"}}>
-                                            <h2>O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento.</h2>
-                                            <p style={{marginTop: 0}}>A REDE GO FAIR Agro e a Comissão Permanente em Vocabulários Controlados, Agroterminologias e Agrossemântica da Embrapa (GTermos) convidam para a mesa-redonda "O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento."</p>
-                                            <p style={{marginTop: 0}}>Todos os interessados no tema podem participar. As inscrições estão abertar e o evento será presencial e híbrido.</p>
-                                            <p style={{marginTop: 0}}><b>Evento presencial</b></p>
-                                            <p style={{marginTop: 0}}>Local: Embrapa Sede, Bloco D, Sala Paule Jeanne. Parque Estaçção Biológica - PqEB, s/nº, Brasília, DF.</p>
-                                            <a id="buttonGreenSmall" href="https://forms.gle/i8cPzQqTdP49bLf4A">Inscreva-se</a>
-                                        </div>
+                                    <div className="newCard" style={{width: "100%", justifyContent: "space-evenly"}}>
+                                        <h2>Apresentação da Rede GO FAIR Agro Brasil na 38ª Reunião da CBAPD</h2>
+                                        <p style={{ marginTop: 0 }}>Na quarta-feira, 16 de outubro de 2024, Sylvia Christina Sanchez, da Brasscom, apresentou o Plano Brasil Digital 2030+ durante a 38ª Reunião Ordinária da Comissão Brasileira de Agricultura de Precisão e Digital (CBAPD). O evento contou com a presença de Sergio Sgobbi e Alexandre Conceição e foi conduzido por Christian Bredemeier. Débora Drucker, da EMBRAPA Agricultura Digital, destacou a Rede GO FAIR Agro Brasil, ressaltando sua importância na agricultura digital. O plano, desenvolvido em colaboração com vários setores, visa acelerar a transformação digital no país.</p>
+                                        <a id="buttonGreenSmall" href=" https://www.linkedin.com/posts/brasscom_paratodosverem-brasscom-macrossetordetic-activity-7254239901196451840-MkL9/?utm_source=share&utm_medium=member_desktop">Saiba mais</a>
+                                    </div>
 
-                                        <div className="newCard">
-                                            <h2>Palestra com Clement Jonquet: Agroportal - Repositório de Vocabulários e Ontologias para Agronomia</h2>
-                                            <p style={{marginTop: 0}}>Estão abertas as inscrições para a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet na próxima terça-feira, dia 17 de outubro. Todos os interessados no tema de construção, organização, disponibilização e uso de ontologias poderão tirar proveito da fala do especialista francês que discutirá na sua palestra as questões relacionadas aos princípios FAIR e à construção e organização de ontologias, por meio da apresentação do AgroPortal e OntoPortal. A palestra é gratuita e será on-line, em inglês, somente para inscritos.</p>
-                                            {/* <a id="buttonGreenSmall" href="https://forms.gle/uY78KywEbQbnbAzc7">Inscreva-se</a> */}
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </div>
-                            
-                        </Carousel.Item>
+                                </div>
+                            </Carousel.Caption>
+                        </div>
 
-                        <Carousel.Item>
-                            <div className="postCarouselItem">
-                                <Carousel.Caption>
-                                    <div className="newCardDeck">
-                                        <div className="newCard">
-                                            <h2>Sucesso no Workshop SmartFood: Análise de Redes Lexicais para Agricultura Digital</h2>
-                                            <p style={{marginTop: 0}}>Parabéns a todos! Nosso grupo de trabalho Ontologias teve mais uma conquista incrível. Um de nossos trabalhos foi aprovado no "Workshop on Controlled Vocabularies and Data platforms for Smart Food Systems (SmartFood)" com o título "Unveiling Knowledge Organization Systems' Artifacts for Digital Agriculture with Lexical Network Analysis". Isso demonstra nosso comprometimento com a pesquisa e a inovação em agricultura digital. Continuamos avançando e contribuindo para o desenvolvimento do campo. Parabéns a todos os envolvidos por mais essa realização!</p>
-                                        </div>
+                    </Carousel.Item>
 
-                                        <div className="newCard">
-                                            <h2>Aniversário de 25 Anos da Scielo e Sessão Especial do GO FAIR Brasil</h2>
-                                            <p style={{marginTop: 0}}>Não poderíamos deixar passar em branco! Na próxima semana, celebraremos o aniversário de 25 anos da Scielo em São Paulo. Vale destacar que a Rede GO FAIR Brasil nasceu no aniversário de 20 anos da Scielo, e este ano não será diferente. Teremos uma sessão especial dedicada ao GO FAIR Brasil. Embora tenhamos sido informados em cima da hora e ainda não tenhamos todos os detalhes, parece que esta sessão será exclusiva para os participantes do evento e exigirá inscrição paga. </p>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
-                <div className="postCarouselMobile">
-                    <Carousel data-bs-theme="dark">
-                        <Carousel.Item>
-                            <div className="postCarouselItem">
-                                <Carousel.Caption>
-                                    <div className="newCardDeck">
-                                        <div className="newCard" style={{textAlign: "left"}}>
-                                            <h2>O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento.</h2>
-                                            <p style={{marginTop: 0}}>A REDE GO FAIR Agro e a Comissão Permanente em Vocabulários Controlados, Agroterminologias e Agrossemântica da Embrapa (GTermos) convidam para a mesa-redonda "O impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento."</p>
-                                            <p style={{marginTop: 0}}>Todos os interessados no tema podem participar. As inscrições estão abertar e o evento será presencial e híbrido.</p>
-                                            <p style={{marginTop: 0}}><b>Evento presencial</b></p>
-                                            <p style={{marginTop: 0}}>Local: Embrapa Sede, Bloco D, Sala Paule Jeanne. Parque Estaçção Biológica - PqEB, s/nº, Brasília, DF.</p>
-                                            <a id="buttonGreenSmall" href="https://forms.gle/i8cPzQqTdP49bLf4A">Inscreva-se</a>
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </div>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <div className="postCarouselItem">
-                                <Carousel.Caption>
-                                    <div className="newCardDeck">
-                                        <div className="newCard">
-                                            <h2>Palestra com Clement Jonquet: Agroportal - Repositório de Vocabulários e Ontologias para Agronomia</h2>
-                                            <p style={{marginTop: 0}}>Estão abertas as inscrições para a palestra "FAIR DATA requires FAIR ONTOLOGIES, how do we do?" ministrada pelo professor doutor Clement Jonquet na próxima terça-feira, dia 17 de outubro. Todos os interessados no tema de construção, organização, disponibilização e uso de ontologias poderão tirar proveito da fala do especialista francês que discutirá na sua palestra as questões relacionadas aos princípios FAIR e à construção e organização de ontologias, por meio da apresentação do AgroPortal e OntoPortal. A palestra é gratuita e será on-line, em inglês, somente para inscritos.</p>
-                                            {/* <a id="buttonGreenSmall" href="https://forms.gle/uY78KywEbQbnbAzc7">Inscreva-se</a> */}
-                                        </div>
-                                    </div>
-                                </Carousel.Caption>
-                            </div>
-                        </Carousel.Item>
+                    <Carousel.Item>
+                        <div className="postCarouselItem">
+                            <Carousel.Caption>
+                                <div className="newCardDeck">
 
-                        <Carousel.Item>
-                            <div className="postCarouselItem">
-                                <Carousel.Caption>
-                                    <div className="newCardDeck">
-                                        <div className="newCard">
-                                            <h2>Sucesso no Workshop SmartFood: Análise de Redes Lexicais para Agricultura Digital</h2>
-                                            <p style={{marginTop: 0}}>Parabéns a todos! Nosso grupo de trabalho Ontologias teve mais uma conquista incrível. Um de nossos trabalhos foi aprovado no "Workshop on Controlled Vocabularies and Data platforms for Smart Food Systems (SmartFood)" com o título "Unveiling Knowledge Organization Systems' Artifacts for Digital Agriculture with Lexical Network Analysis". Isso demonstra nosso comprometimento com a pesquisa e a inovação em agricultura digital. Continuamos avançando e contribuindo para o desenvolvimento do campo. Parabéns a todos os envolvidos por mais essa realização!</p>
-                                        </div>
+                                    <div className="newCard">
+                                        <h2>Sucesso no Workshop SmartFood: Análise de Redes Lexicais para Agricultura Digital</h2>
+                                        <p style={{ marginTop: 0 }}>Parabéns a todos! Nosso grupo de trabalho Ontologias teve mais uma conquista incrível. Um de nossos trabalhos foi aprovado no "Workshop on Controlled Vocabularies and Data platforms for Smart Food Systems (SmartFood)" com o título "Unveiling Knowledge Organization Systems' Artifacts for Digital Agriculture with Lexical Network Analysis". Isso demonstra nosso comprometimento com a pesquisa e a inovação em agricultura digital. Continuamos avançando e contribuindo para o desenvolvimento do campo. Parabéns a todos os envolvidos por mais essa realização!</p>
                                     </div>
-                                </Carousel.Caption>
-                            </div>
-                        </Carousel.Item>
 
-                        <Carousel.Item>
-                            <div className="postCarouselItem">
-                                <Carousel.Caption>
-                                    <div className="newCardDeck">
-                                        <div className="newCard">
-                                            <h2>Aniversário de 25 Anos da Scielo e Sessão Especial do GO FAIR Brasil</h2>
-                                            <p style={{marginTop: 0}}>Não poderíamos deixar passar em branco! Na próxima semana, celebraremos o aniversário de 25 anos da Scielo em São Paulo. Vale destacar que a Rede GO FAIR Brasil nasceu no aniversário de 20 anos da Scielo, e este ano não será diferente. Teremos uma sessão especial dedicada ao GO FAIR Brasil. Embora tenhamos sido informados em cima da hora e ainda não tenhamos todos os detalhes, parece que esta sessão será exclusiva para os participantes do evento e exigirá inscrição paga. </p>
-                                        </div>
+                                    <div className="newCard">
+                                        <h2>Aniversário de 25 Anos da Scielo e Sessão Especial do GO FAIR Brasil</h2>
+                                        <p style={{ marginTop: 0 }}>Não poderíamos deixar passar em branco! Na próxima semana, celebraremos o aniversário de 25 anos da Scielo em São Paulo. Vale destacar que a Rede GO FAIR Brasil nasceu no aniversário de 20 anos da Scielo, e este ano não será diferente. Teremos uma sessão especial dedicada ao GO FAIR Brasil. Embora tenhamos sido informados em cima da hora e ainda não tenhamos todos os detalhes, parece que esta sessão será exclusiva para os participantes do evento e exigirá inscrição paga. </p>
                                     </div>
-                                </Carousel.Caption>
-                            </div>
-                        </Carousel.Item>
-                    </Carousel>
-                </div>
+                                </div>
+                            </Carousel.Caption>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <div className="postCarouselMobile">
+                <Carousel data-bs-theme="dark">
+
+                    <Carousel.Item>
+                        <div className="postCarouselItem">
+                            <Carousel.Caption>
+                                <div className="newCardDeck">
+                                    <div className="newCard">
+                                        <h2>Apresentação da Rede GO FAIR Agro Brasil na 38ª Reunião da CBAPD</h2>
+                                        <p style={{ marginTop: 0 }}>Na quarta-feira, 16 de outubro de 2024, Sylvia Christina Sanchez, da Brasscom, apresentou o Plano Brasil Digital 2030+ durante a 38ª Reunião Ordinária da Comissão Brasileira de Agricultura de Precisão e Digital (CBAPD). O evento contou com a presença de Sergio Sgobbi e Alexandre Conceição e foi conduzido por Christian Bredemeier. Débora Drucker, da EMBRAPA Agricultura Digital, destacou a Rede GO FAIR Agro Brasil, ressaltando sua importância na agricultura digital. O plano, desenvolvido em colaboração com vários setores, visa acelerar a transformação digital no país.</p>
+                                        <a id="buttonGreenSmall" href=" https://www.linkedin.com/posts/brasscom_paratodosverem-brasscom-macrossetordetic-activity-7254239901196451840-MkL9/?utm_source=share&utm_medium=member_desktop">Saiba mais</a>
+                                    </div>
+                                </div>
+                            </Carousel.Caption>
+                        </div>
+                    </Carousel.Item>
+
+
+                    <Carousel.Item>
+                        <div className="postCarouselItem">
+                            <Carousel.Caption>
+                                <div className="newCardDeck">
+                                    <div className="newCard">
+                                        <h2>Sucesso no Workshop SmartFood: Análise de Redes Lexicais para Agricultura Digital</h2>
+                                        <p style={{ marginTop: 0 }}>Parabéns a todos! Nosso grupo de trabalho Ontologias teve mais uma conquista incrível. Um de nossos trabalhos foi aprovado no "Workshop on Controlled Vocabularies and Data platforms for Smart Food Systems (SmartFood)" com o título "Unveiling Knowledge Organization Systems' Artifacts for Digital Agriculture with Lexical Network Analysis". Isso demonstra nosso comprometimento com a pesquisa e a inovação em agricultura digital. Continuamos avançando e contribuindo para o desenvolvimento do campo. Parabéns a todos os envolvidos por mais essa realização!</p>
+                                    </div>
+                                </div>
+                            </Carousel.Caption>
+                        </div>
+                    </Carousel.Item>
+
+                    <Carousel.Item>
+                        <div className="postCarouselItem">
+                            <Carousel.Caption>
+                                <div className="newCardDeck">
+                                    <div className="newCard">
+                                        <h2>Aniversário de 25 Anos da Scielo e Sessão Especial do GO FAIR Brasil</h2>
+                                        <p style={{ marginTop: 0 }}>Não poderíamos deixar passar em branco! Na próxima semana, celebraremos o aniversário de 25 anos da Scielo em São Paulo. Vale destacar que a Rede GO FAIR Brasil nasceu no aniversário de 20 anos da Scielo, e este ano não será diferente. Teremos uma sessão especial dedicada ao GO FAIR Brasil. Embora tenhamos sido informados em cima da hora e ainda não tenhamos todos os detalhes, parece que esta sessão será exclusiva para os participantes do evento e exigirá inscrição paga. </p>
+                                    </div>
+                                </div>
+                            </Carousel.Caption>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
         </div>
     );
 }
+
+
+
 
