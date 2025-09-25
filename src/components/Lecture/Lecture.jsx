2f7148e2a4fdabb1061482bf01a32bce0e672e72
maxcravo <maxcravo25@gmail.com>
2025-09-25 19:47:27 -0300

    new Lecture added

--- src/components/Lecture/Lecture.jsx
+++ src/components/Lecture/Lecture.jsx
@@ -6,7 +6,7 @@ import palestra5 from "../../assets/palestra5.png";
 import palestra6 from "../../assets/mesa_redonda_ontologias.png";
 import palestra7 from "../../assets/WRNP_2025.png"
 import palestra8 from "../../assets/biblioCLM_palestra9.png"
-
+import palestra9 from "../../assets/UX_sistema_palestra_10.png"
 
 import ytb from "../../assets/ytb.png";
 import pdfPng from "../../assets/download.png";
@@ -16,6 +16,7 @@ import pdf3 from "../../assets/24-04-24_Embrapa_OntoPortal_Jonquet.pdf";
 import pdf4 from "../../assets/2024-10-17-Tools_to_characterize_FAIR_and_measure_FAIRness.pdf";
 import pdf5 from "../../assets/esquemas_de_metadados_fair.pdf";
 import pdf6 from "../../assets/mesa_redonda_ontologias.pdf";
+import pdf7 from "../../assets/GoFAIR_UX_onto.pdf";
 
 // import pdf5 from "../../assets/esquemas_de_metadados_fair.pdf";
 
@@ -29,6 +30,23 @@ export const Lecture = () => {
             <h1>Palestras</h1>
             <div className="cycleOficinasContainer">
                 <div className="cycleOficinasDeck">
+
+                     <div className="cycleCard">
+                        <p id="cycleCardTitle">UX em Sistemas de Organização do Conhecimento: como melhorar ontologias, tesauros e taxonomias com testes de usabilidade</p>
+                        <img src={palestra9} alt="biblioCLM" />
+                        <p>Acesse o material:</p>
+                        <div className="cardDeckLinks">
+                            <a target="blank" href="https://www.youtube.com/watch?v=kGlG1i8w5u4">
+                                <img id="linkYtb" src={ytb} alt="" />
+                                <p>Youtube</p>
+                            </a>
+                            <a href={pdf7} download={"GoFAIR_UX_onto.pdf"}>
+                                <img id="linkPDF" src={pdfPng} alt="" />
+                                <p>PDF</p>
+                            </a>
+                        </div>
+                    </div>
+
 
                     <div className="cycleCard">
                         <p id="cycleCardTitle">Mesa-redonda Ontologias: o que são e como aplicar, com Patricia Ferreira da Silva e Mara Abel (2025)</p>
@@ -98,7 +116,7 @@ export const Lecture = () => {
 
 
                     <div className="cycleCard">
-                        <p id="cycleCardTitle">Palestra com Fernanda Farinelli: Conectando Mundos - Desafios Semânticos, Interoperabilidade e Ontologias de Domínio (2024)</p>
+                        <p id="cycleCardTitle">Mesa-redonda: Impacto das ontologias na interoperabilidade semântica para gestão de dados, informação e conhecimento. Professora Fernanda Farinelli (UnB) e Clement Jonquet (INRAE) (2024).</p>
                         <img src={palestra2} alt="" />
                         <p>Acesse o material:</p>
                         <div className="cardDeckLinks">
@@ -145,25 +163,10 @@ export const Lecture = () => {
                         </div>
                     </div>
 
-                     <div className="cycleCard">
-                        <p id="cycleCardTitle">BiblioCLM | O futuro do Agro é FAIR: dados abertos e a produtividade em Ciências Agrárias</p>
-                        <img src={palestra8} alt="biblioCLM" />
-                        <p>Acesse o material:</p>
-                        <div className="cardDeckLinks">
-                            <a target="blank" href="https://www.youtube.com/live/U6iRuO5nHpE">
-                                <img id="linkYtb" src={ytb} alt="" />
-                                <p>Youtube</p>
-                            </a>
-                            <a href={""}>
-                                <img id="linkPDF" src={pdfPng} alt="" />
-                                <p>Em Breve</p>
-                            </a>
-                        </div>
-                    </div>
-
 
                     <div id="palestraCard">
                         <p style={{ color: "#2D6D2C" }} id="cycleCardTitle">Clique nos links abaixo para conferir mais palestras:</p>
+                        <a target="blank" href="https://www.youtube.com/live/U6iRuO5nHpE" id="linkPalestras">BiblioCLM | O futuro do Agro é FAIR: dados abertos e a produtividade em Ciências Agrárias</a>
                         <a target="blank" href="https://drive.google.com/file/d/1eAwKKLoillH0BnFzQC2y5leC-DbwFa2n/view?usp=sharing" id="linkPalestras">O Processo de Construção da Política de Governança de Dados, Informação e Conhecimento da Embrapa</a>
                         <a target="blank" href="https://drive.google.com/file/d/1Re3OZHgJXrEnhyek0coG1mAYaKlmiUAk/view?usp=sharing" id="linkPalestras">Plataformas para Criação de Planos de Gestão de Dados Usando os Princípios FAIR</a>
                         <a target="blank" href="https://drive.google.com/file/d/1i5FkAPpVD9baQkUThxNB0yCRMs2IjlvN/view?usp=sharing" id="linkPalestras">Planos de Gestão de Dados Acionáveis por Máquina Alinhados aos Princípios FAIR no LMB da Embrapa </a>
